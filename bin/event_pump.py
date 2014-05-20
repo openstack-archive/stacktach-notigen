6e01a84104ebb5d12b260c7ac3643c853b83d0a2
Sandy Walsh <sandy@sandywalsh.com>
2014-05-20 20:23:57 +0000

    started working on send to rabbit utility

---
+++ bin/event_pump.py
@@ -0,0 +1,29 @@
+import datetime
+
+from notabene import kombu_driver as driver
+import notigen
+
+"""Pump simulated OpenStack notificationss into RabbitMQ.
+
+You need to install rabbitqm-server and
+pip install librabbitmq
+pip install notabene
+"""
+
+
+connection = driver.create_connection("localhost", 5672, 'guest', 'guest',
+                                      "librabbitmq", "/")
+exchange = driver.create_exchange("monitor", "topic")
+
+g = notigen.EventGenerator(100)  # Number of operations per minute
+now = datetime.datetime.utcnow()
+start = now
+nevents = 0
+while nevents < 10000:
+    e = g.generate(now)
+    if e:
+        nevents += len(e)
+        for event in e:
+            driver.send_notification(event, "monitor.info", connection, exchange)
+
+    now = datetime.datetime.utcnow()
