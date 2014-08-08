a6d93a93b116781585e46f72c95056ffe698fea1
Sandy Walsh <sandy@sandywalsh.com>
2014-08-08 13:07:08 -0700

    phew ... datetime mapping and output in place

--- bin/templategen.py
+++ bin/templategen.py
@@ -17,6 +17,18 @@ import dateutil.parser
 import mysql.connector
 import notification_utils as nu
 
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            encoded_object = list(obj.timetuple())[0:7]
+        elif isinstance(obj, datetime.timedelta):
+            encoded_object = (obj.days, obj.seconds, obj.microseconds)
+        else:
+            encoded_object =json.JSONEncoder.default(self, obj)
+        return encoded_object
+
+
 def get_all_events(cnx, date_range, next_range):
     # Get all the events (including EOD .exists) for a day
 
@@ -184,7 +196,7 @@ def placeholder(value, original, context, key, template, parent):
         context[key] = mappings
     value = value % index
     parent(value)
-    return value
+    return value, index
 
 
 def _replace_list(data, index):
@@ -226,18 +238,48 @@ regex_list = [(uuid_regex, "uuid", "[[[[UUID_%d]]]]"),
               (dt2_regex, "dt", "[[[[DT_%d]]]]"),
               ]
 
+protect = {"tenant_id": ["_context_project_id", "_context_project_name",
+                         "_context_tenant", "tenant_id"],
+           "user_id": ["_context_user", "_context_user_id",
+                        "_context_user_name", "user_id"],
+           "display_name": ["display_name",],
+           "host": ["host",],
+           "hostname": ["hostname",],
+           "node": ["node",],
+           "reservation_id": ["reservation_id",],
+           "publisher_id": ["publisher_id",]}
+
 
 def scrub(context, struct, parent):
     #print struct, type(struct)
     if type(struct) is list:
         #print "List"
+        to_delete = []
         for index, x in enumerate(struct):
             scrub(context, x, _replace_list(struct, index))
+            if type(x) in [unicode, str]:
+                if 'rax' in x or 'rackspace' in x:
+                    # reverse insert order so we can safely delete later.
+                    to_delete.insert(0, index)
+
+        for index in to_delete:
+            struct.pop(index)
+
     elif type(struct) is dict:
         #print "Dict"
+        to_delete = []
         for k, v in struct.iteritems():
+            if "password_info" in k or "rax" in k or "rackspace" in k:
+                to_delete.append(k)
+                continue
+            for watch_key, watch_list in protect.iteritems():
+                if k in watch_list:
+                    struct[k] = "[[[[%s]]]]" % watch_key
+                    continue
             scrub(context, v, _replace_dict_value(struct, k))
             scrub(context, k, _replace_dict_key(struct, v, k))
+        for k in to_delete:
+            del struct[k]
     elif type(struct) in [unicode, str]:
         #print "Str:", struct
         for regex, pkey, ptemplate in regex_list:
@@ -246,16 +288,47 @@ def scrub(context, struct, parent):
                 source = struct
                 original = struct[m.start():m.end()]
                 full_template = struct[:m.start()] + ptemplate + struct[m.end():]
-                print "Found %s:" % pkey, source, original, struct
-                struct = placeholder(full_template, original, context, pkey, ptemplate, parent)
+                #print "Found %s:" % pkey, source
+                struct, index = placeholder(full_template, original, context, pkey,
+                                            ptemplate, parent)
+                if pkey == 'dt':
+                    start = context['_start_time']
+                    time_map = context['_time_map']
+                    now = dateutil.parser.parse(original)
+                    time_map[ptemplate % index] = now - start
 
 
 for stream_type, streams_by_len in patterns.iteritems():
+    output = []
     for length, stream in streams_by_len.iteritems():
-        context = {}
+        context = {'_time_map': {}}
         for when, uuid, event, rawjson in stream:
+            if not context.get('_start_time'):
+                context['_start_time'] = dateutil.parser.parse(rawjson['timestamp'])
             scrub(context, rawjson, None)
-            print json.dumps(rawjson, sort_keys=True, indent=4)
-            sys.exit(1)
+            output.append(rawjson)
+            #print json.dumps(rawjson, sort_keys=True, indent=4)
 
+        #print json.dumps(context, cls=DateTimeEncoder, sort_keys=True, index=4)
+    output.insert(0, {'time_map': context['_time_map']})
+
+    filename = "templates/%s_%d.json" % (stream_type, length)
+    with open(filename, "w") as f:
+        json.dump(output, f, cls=DateTimeEncoder, sort_keys=True, indent=4)
+
+    timemap = output[0]['time_map']
+    now = datetime.datetime.utcnow()
+    this_context = {}
+    for k, td in timemap.iteritems():
+        this_context[k] = now + td
+    for item in output[1:]:
+        now.time.replace(datetime.time.min())
+        string = json.dumps(item, cls=DateTimeEncoder)
+        for k, v in this_context:
+            string = string.replace(k, str(v))
+        x = json.loads(string)
+        print json.dumps(x, cls=DateTimeEncoder, sort_keys=Trye, indent=4)
+        sys.exit(1)
+
+    sys.exit(1)
 cnx.close()
